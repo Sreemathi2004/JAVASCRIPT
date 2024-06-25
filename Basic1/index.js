2a8a5d1338f0b7b1e176a3c70dc3a25b7bd87c0e
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-06-25 22:00:29 +0530

    commit

--- Basic1/index.js
+++ Basic1/index.js
@@ -1,1 +1,88 @@
-console.log("Hi")
+// Assignment 1
+
+let name ="Sreemathi";
+let whatDoYouWannaBecomeInYourLife="Programmer";
+
+let gender="female";
+let twitterHandle ="twitterHandle";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
+
+
+// Numbers 
+
+let num=-10;
+console.log(num);
+console.log(typeof num);
+
+//BASIC MATH
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let counter=10;
+counter++;
+counter--;
+counter--;
+
+console.log(counter);
+
+//Assignment2
+
+let firstFavNum =1;
+let secondFavNum=2;
+console.log(firstFavNum+secondFavNum);
+console.log(secondFavNum-firstFavNum);
+console.log(firstFavNum/secondFavNum);
+console.log(firstFavNum%secondFavNum);
+console.log(firstFavNum**secondFavNum);
+console.log("sree");
+
+//Assignment3
+let isJsProgrammingLanguage=true;
+let isJsHard=false;
+let favNumb=2;
+console.log(favNumb+undefined);//NAN
+
+//Equality operator
+console.log(10===10);
+console.log(10==4);
+console.log(10=='10');
+console.log(10==='10');
+console.log(10!==9);
+console.log(10!=10);
+console.log(10!=9);
+
+//Assignment4
+
+let firstFavNum_1=4;
+let secondFavNum_1=6;
+
+console.log(firstFavNum_1>secondFavNum_1);
+console.log(firstFavNum_1<secondFavNum_1);
+console.log(firstFavNum_1>=secondFavNum_1);
+console.log(firstFavNum_1<=secondFavNum_1);
+console.log(firstFavNum_1===secondFavNum_1);
+console.log(firstFavNum_1!==secondFavNum_1);
+console.log(firstFavNum_1==secondFavNum_1);
+console.log(firstFavNum_1!=secondFavNum_1);
+
+
+//String
+
+console.log("Sreemathi".split("").join("-"));
+console.log("Sreemathi".includes("i"));
+console.log("   Sree    ".trim());
+
+//Assignment5
+let favActorFirstName="Vishnu";
+let favActorLastName="Varadhan";
+let fullName=favActorFirstName.concat(favActorLastName);
+console.log(fullName);
+console.log(fullName.toUpperCase());
