50e6e52cfcff6ac90876aca3137f6fe02423f557
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-06-28 07:38:52 +0530

    added

--- DOM/Events/app.js
+++ DOM/Events/app.js
@@ -1,5 +1,10 @@
 // ----------- BAD WAY-----------
-
+const secondBtn = document.querySelector(".second-btn");
+secondBtn.onmousemove=()=>console.log("SreeVishnu");
+secondBtn.onmousemove=()=>alert("Hello");
 // ----------- GREAT WAY-----------
+
+const best=document.querySelector(".best");
+best.addEventListener("click",()=>console.log("hi hello"));
 
 // ----------- Event (e) Object -----------
